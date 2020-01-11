b8af75211ea37036f4e7af7cd1e47eac2e20ccf4
Sean Hellum <seanhellum45@gmail.com>
2020-01-11 19:29:20 +0000

    update

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,13 @@
 import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
     console.log('Congratulations, your extension "helloworld" is now active!');
-    let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+    context.subscriptions.push(vscode.commands.registerCommand('extension.ls', () => {
+        const terminal = vscode.window.createTerminal(`ls command`);
+        terminal.sendText("ls");
+        terminal.show(true);
+    }));
+    context.subscriptions.push(vscode.commands.registerCommand('extension.helloWorld', () => {
         vscode.window.showInformationMessage('Hello World From VSCode!');
-    });
-
-    context.subscriptions.push(disposable);
+    }));
 }
 export function deactivate() { }
